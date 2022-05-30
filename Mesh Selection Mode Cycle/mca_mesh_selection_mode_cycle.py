ec6d5e7eaf2b625d441dfdc7cc607d0465c06716
neverwinter-nights <71729548+neverwinter-nights@users.noreply.github.com>
2022-05-30 23:56:31 +0300

    ...

---
+++ Mesh Selection Mode Cycle/mca_mesh_selection_mode_cycle.py
@@ -0,0 +1,158 @@
+# mca_mesh_selection_mode_cycle.py.
+
+# Mesh Selection Mode Cycle.
+
+bl_info = {
+    "name": "Mesh Selection Mode Cycle",
+    "description": "Cycle through mesh selection modes. Works when a single mode is selected.",
+    "category": "Mesh",
+    "blender": (3, 1, 0),
+    "author": "McArcher",
+}
+
+import bpy
+
+class MSM:
+    # Number of possible mesh selection modes. In Blender 3.1 there are three
+    # modes: Vertex, Edge, Face.
+    Count = 3
+
+    DirectionPrevious = 'prev'
+    DirectionNext = 'next'
+
+    ResultCancel = 'CANCELLED'
+    ResultSuccess = 'FINISHED'
+
+    MeshEditMode = 'EDIT_MESH'
+
+
+class McaMeshSelectionModePrevious(bpy.types.Operator):
+    """Mesh Selection Mode Cycle: Previous"""
+    bl_idname = "object.mca_mesh_selection_mode_previous"
+    bl_label = "Mesh Selection Mode Cycle: Previous"
+
+    @classmethod
+    def poll(cls, context):
+        return is_mesh_edit_mode_selected()
+
+    def execute(self, context):
+        return execute_generic(MSM.DirectionPrevious)
+
+
+class McaMeshSelectionModeNext(bpy.types.Operator):
+    """Mesh Selection Mode Cycle: Next"""
+    bl_idname = "object.mca_mesh_selection_mode_next"
+    bl_label = "Mesh Selection Mode Cycle: Next"
+
+    @classmethod
+    def poll(cls, context):
+        return is_mesh_edit_mode_selected()
+
+    def execute(self, context):
+        return execute_generic(MSM.DirectionNext)
+
+
+def register():
+    bpy.utils.register_class(McaMeshSelectionModePrevious)
+    bpy.utils.register_class(McaMeshSelectionModeNext)
+
+
+def unregister():
+    bpy.utils.unregister_class(McaMeshSelectionModeNext)
+    bpy.utils.unregister_class(McaMeshSelectionModePrevious)
+
+
+# Counts the enabled mesh selection modes. Returns -1 on error. Argument (msm)
+# is an array. Documentation:
+# https://docs.blender.org/api/current/bpy.types.ToolSettings.html#bpy.types.ToolSettings.mesh_select_mode
+def count_enebled_msm(msm):
+    # Is everything fine ?
+    if len(msm) != MSM.Count:
+        return -1
+
+    enabled_msm_count = 0
+    for idx in range(MSM.Count):
+        if msm[idx] == True:
+            enabled_msm_count += 1
+
+    return enabled_msm_count
+
+
+# Gets the first enabled mode (index).
+# Returns -1 on error.
+def get_first_selected_msm(msm):
+    for idx in range(MSM.Count):
+        if msm[idx] == True:
+            return idx
+
+    return -1
+
+
+# Returns the previous mesh selection mode (array index).
+def get_previous_msm(cur_mode):
+    if cur_mode <= 0:
+        return MSM.Count - 1
+
+    return cur_mode - 1
+
+
+# Returns the next mesh selection mode (array index).
+def get_next_msm(cur_mode):
+    if cur_mode >= MSM.Count - 1:
+        return 0
+
+    return cur_mode + 1
+
+
+# Selects (applies) the mesh selection mode.
+def select_msm(msm_idx):
+    msm = []
+    for i in range(MSM.Count):
+        msm.append(False)
+
+    msm[msm_idx] = True
+
+    bpy.context.tool_settings.mesh_select_mode = msm
+
+
+# Checks whether the mesh edit mode is selected.
+def is_mesh_edit_mode_selected():
+    return bpy.context.mode == MSM.MeshEditMode
+
+
+# A generic method to execute various actions. Cycles through mesh selection
+# modes. Changes the selected mode to a next or a previous mode if a single
+# mode is selected. Does nothing if more than one mode is selected. Selects the
+# first mode if no mode is selected.
+def execute_generic(direction):
+    if (direction != MSM.DirectionPrevious) and \
+            (direction != MSM.DirectionNext):
+        return {MSM.ResultCancel}
+
+    msm = bpy.context.tool_settings.mesh_select_mode
+    selections_count = count_enebled_msm(msm)
+
+    # Exit on error.
+    if selections_count < 0:
+        return {MSM.ResultCancel}
+
+    # Exit if more than one mode is selected.
+    if selections_count > 1:
+        return {MSM.ResultCancel}
+
+    # Select the first mode if no mode is selected.
+    if selections_count == 0:
+        select_msm(0)
+        return {MSM.ResultSuccess}
+
+    # Select the next or previous mode if a single mode is selected.
+    selected_idx = get_first_selected_msm(msm)
+
+    new_idx = -1
+    if direction == MSM.DirectionPrevious:
+        new_idx = get_previous_msm(selected_idx)
+    if direction == MSM.DirectionNext:
+        new_idx = get_next_msm(selected_idx)
+
+    select_msm(new_idx)
+    return {MSM.ResultSuccess}
--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# Mesh-Selection-Mode-Cycle
-An add-on for Blender 3D editor.
+# Mesh Selection Mode Cycle
+An add-on for Blender 3D editor, version 3.x.
+
+## Description
+This add-on enables fast switching between mesh selection modes.  
+The add-on provides two actions to which buttons can be assigned:
+* **object.mca_mesh_selection_mode_previous** – switch to a previous mesh selection mode; 
+* **object.mca_mesh_selection_mode_next** – switch to a next mesh selection mode.
+
+Mode switch happens only if a single selection mode is enabled. E.g., if you 
+enable two selection modes at the same time, the add-on will not do any changes 
+to the enabled selection modes.
+
+## Why ?
+The original, built into Blender, method of switching between different 
+selection modes in the mesh editing process is quite difficult. It requires a 
+user to look at the keyboard, remember which button is which mode and then 
+press a button `1`, `2` or `3` depending on a mode selected. Some other 3D 
+editors, like the Lightwave 3D editor, provide another, a much more convenient, 
+way of changing the selection mode. Which method ? Cycling through available 
+modes with a key which is very easy to use. This add-on tries to implement the 
+same functionality as in the Lightwave 3D editor in the Blender editor. With 
+this add-on you can, for example, switch between selection modes with just a 
+single key of a keyboard or a mouse.  
+
+## Installation
+1. Start the Blender. Make sure that Blender's version is **3.1** or later.
+2. Open menu: **Edit** -> **Preferences** -> **Add-ons** -> `Install...`
+3. Select the python file (*.py) with this add-on and press the `Install Add-on` button.  
+The add-on will be added into a **Mesh** category.
+4. Find the add-on in the list and enable it with a checkbox.
+5. In the same **Preferences** window select the **Keymap** on the left side.
+6. Navigate to the **3D View** -> **Mesh** -> **Mesh (Global)** section.
+7. Press `Add New` in this section. A **none** item will appear.
+8. Modify the empty item changing its **none** action to one of the following: 
+* **object.mca_mesh_selection_mode_previous** – if you want to switch to a previous mesh selection mode; 
+* **object.mca_mesh_selection_mode_next** – if you want to switch to a next mesh selection mode.
+10. Assign a button to the action. It may be a keyboard key, a mouse button or anything you like.
+11. Enjoy the Blender editor which is now more comfortable to use.
+
+Below is an example of assignment of `Alt` + `Mouse Scroll Wheel` to both actions.   
+Screenshot's width is about 1000 px. To see it, you might need to open the page in a full screen.
+![Keymap](./Screenshot/key_binding_example.png)
+
+## Usage
+Enter the mesh editing mode. Press your key combination.  
+If you had a single selection mode enabled, it will be switched to a next or a 
+previous mesh selection mode depending on your settings. 
+
+## Notes
+Unfortunately, modern version of Blender editor have bugs with controls. For 
+some reason the developers of Blender decided to assign hundreds of actions to 
+the **Press** event instead of the **Click** event. If your newly assigned key 
+does not work, do check if there are any **Press** events assigned to the same 
+key. For some reason **Press** events hijack other events and break the program. 
+Me myself had to spend hours searching for the problem cause, and I had to 
+re-assign hundreds of bindings to make my new bindings work. Be aware!
